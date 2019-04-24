2fb9946cf5421d8fffed1be0f5187ae25c108087
Stubean <foleystew@gmail.com>
2019-04-23 23:02:22 -0400

    Updated the color of the website and added notes

--- src/Pages/Work.js
+++ src/Pages/Work.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 class Work extends Component {
-    render() {
-      return (
-        <div>
-          <div>
-            Look at all this work!
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <div className="col">
+            Design System
           </div>
         </div>
-      );
-    }
+        <div className="row">
+          <div className="col">
+            Mobile Work
+        </div>
+        </div>
+      </div>
+    );
   }
-  
-  export default Work;
-  
+}
+
+export default Work;
