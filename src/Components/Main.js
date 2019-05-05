d24aaaba4cd1484ef6f674f87237b52970bf56f0
Stubean <foleystew@gmail.com>
2019-05-05 10:22:30 -0400

    Worked on colors and home page

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,13 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 import Work from '../Pages/Work.js';
 import About from '../Pages/About';
 import Contact from '../Pages/Contact';
+import Home from '../Pages/Home';
 
 class Main extends Component {
     render() {
       return (
         <main>
         <Switch>
-          <Route exact path='/' component={Work}/>
+          <Route exact path='/' component={Home}/>
           <Route path='/work' component={Work}/>
           <Route path='/about' component={About}/>
           <Route path='/contact' component={Contact}/>
---
+++ src/Pages/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../Styles/Home.css'
+
+
+class Home extends Component {
+  render() {
+    return (
+      <div id="home">
+        <div id="mainText">
+            Hi, I'm Stuart :)
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
